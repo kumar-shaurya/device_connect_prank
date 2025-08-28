3612bbd07a80a35c1e0ef82c3b6df2a3ce055e72
Kumar Shaurya <kumar.shaurya1307@gmail.com>
2025-08-28 23:13:05 +0530

    Bug Fix

--- set_sounds.bat
+++ set_sounds.bat
@@ -1,4 +1,4 @@
 @echo off
-reg add "HKCU\AppEvents\Schemes\Apps\.Default\DeviceConnect\.Current" /ve /t REG_SZ /d "C:\SYSOUND\connect.wav" /f
-reg add "HKCU\AppEvents\Schemes\Apps\.Default\DeviceDisconnect\.Current" /ve /t REG_SZ /d "C:\SYSOUND\disconnect.wav" /f
+reg add "HKCU\AppEvents\Schemes\Apps\.Default\DeviceConnect\.Current" /ve /t REG_SZ /d "C:\device_connect_prank\connect.wav" /f
+reg add "HKCU\AppEvents\Schemes\Apps\.Default\DeviceDisconnect\.Current" /ve /t REG_SZ /d "C:\device_connect_prank\disconnect.wav" /f
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
